70de82891378b8b3e71149c21a6167062c813bf0
TriNguyenGit <nguyenvantri51480153@gmail.com>
2020-07-22 19:04:23 +0700

    (Private)

--- app/src/main/java/com/example/random13072020/MainActivity.java
+++ app/src/main/java/com/example/random13072020/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.random13072020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int value = tinhTong(5,10);
+        Log.d("BBB" ,String.valueOf(value));
+    }
+    private int tinhTong(int a , int b){
+        int ketqua = a + b;
+        return ketqua;
     }
 }
