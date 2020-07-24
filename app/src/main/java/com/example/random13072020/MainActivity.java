f6a4be6af3736958bbdeafbe76f3ae4036229fa1
TriNguyenGit <nguyenvantri51480153@gmail.com>
2020-07-24 18:33:24 +0700

    Game random

--- app/src/main/java/com/example/random13072020/MainActivity.java
+++ app/src/main/java/com/example/random13072020/MainActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //global
+    EditText mEditSomin, mEditSomax;
+    Button mBtnRanDom;
+    TextView mTvKetQua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Random random = new Random();
-        int value = random.nextInt(6);
-        Log.d("BBB", value + " ");
+        // local
     }
 
 }
